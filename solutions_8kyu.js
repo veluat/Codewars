514d64d16eb4adc1b4387a87dd92e2696dc0dbbc
Julia <veluat@gmail.com>
2022-08-12 23:19:31 +0300

    Add 20 solutions

--- solutions_8kyu.js
+++ solutions_8kyu.js
@@ -69,3 +69,143 @@ function howMuchILoveYou(nbPetals) {
     return ("not at all");
   }
 }
+//Multiply
+
+function multiply(a, b){
+  return a * b;
+}
+
+//Opposite number
+
+function opposite(number) {
+  return -number;
+}
+
+//Sum of positive
+
+function positiveSum(arr) {
+  let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) sum += arr[i]
+    }
+    return sum
+}
+
+//Remove First and Last Character
+
+function removeChar(str){
+  return str.slice(1, str.length - 1);
+};
+
+//You only need one - Beginner
+
+function check(a, x) {
+  return a.includes(x) ? true : false; 
+}
+
+//Beginner - Lost Without a Map
+
+function maps(x){
+  let newX = [];
+    for(let i = 0; i < x.length; i++){
+        newX.push(x[i] * 2);
+    }
+    return newX; 
+}
+
+//Abbreviate a Two Word Name
+
+function abbrevName(name){
+  const newArray = name.split(" ")
+  return (newArray[0][0] + "." + newArray[1][0]).toUpperCase()
+    }
+
+    //Keep Hydrated!
+
+    function litres(time) {
+      return Math.floor (time/2);
+    }
+
+    //Is he gonna survive?
+
+    function hero(bullets, dragons){
+      let sumBullets = bullets;
+      if (sumBullets >= dragons*2) {
+        return true;
+      } else {
+        return false;
+      }
+      }
+
+//DNA to RNA Conversion
+
+function DNAtoRNA(dna) {
+  let newDna = '';
+for (let i = 0; i < dna.length; i++){
+  if (dna[i] == 'T'){
+    newDna += 'U';
+} else {
+  newDna += dna[i];
+}
+}
+return newDna;
+}
+
+//Transportation on vacation
+
+function rentalCarCost(d) {
+  let totalCost = 0;
+  let costOneDay = 40;
+  if (d<3) {
+    totalCost = costOneDay * d;
+    return totalCost;
+  } else if (d==3 || d<7) {
+    totalCost = costOneDay * d - 20;
+    return totalCost;
+  } else if (d>=7) {
+    totalCost = costOneDay * d - 50;
+    return totalCost;
+}
+}
+
+//Simple multiplication
+
+function simpleMultiplication(number) {
+  let myNumber = number;
+  if(number % 2 == 0) {
+    return(myNumber * 8);
+  } else {
+    return(myNumber * 9);
+}
+}
+
+//Reversed Strings
+
+function solution(str){
+  return str.split("").reverse().join("");
+}
+
+//Convert number to reversed array of digits
+
+function digitize(n) {
+  return ("" + n).split("").map(el => el * 1).reverse()
+}
+
+//Convert a String to a Number!
+
+const stringToNumber = function(str){
+  // put your code here
+  return Number(str);
+}
+
+//Counting sheep...
+
+function countSheeps(arrayOfSheep) {
+  return arrayOfSheep.filter(item => item === true).length
+}
+
+//Grasshopper - Summation
+
+var summation = function (num) {
+  return num*(num+1)/2
+    }
