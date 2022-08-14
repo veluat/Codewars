17b0ca78c778fc5a483994b0022097f60af67409
Julia Popova <109670861+veluat@users.noreply.github.com>
2022-08-15 00:06:35 +0300

    Update solutions_8kyu_part2.js

--- solutions_8kyu_part2.js
+++ solutions_8kyu_part2.js
@@ -42,3 +42,10 @@ function makeNegative(num) {
     return num;
   }
 }
+
+//Alan Partridge II - Apple Turnover
+
+function apple(x){
+  return Math.pow(x, 2) > 1000 ? "It's hotter than the sun!!" 
+  : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
