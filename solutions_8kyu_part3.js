d3320597f456586b6f3527f1b1b089f78b3aab8b
Julia Popova <109670861+veluat@users.noreply.github.com>
2022-08-21 00:07:38 +0300

    Add 3 solutions

--- solutions_8kyu_part3.js
+++ solutions_8kyu_part3.js
@@ -4,3 +4,16 @@ var findAverage = function (nums) {
   let num = nums.reduce((el,sum) => el + sum, 0);
   return num / nums.length;
 }
+
+//Welcome to the City
+
+function sayHello( name, city, state ) {
+  let person = name.join(' ');
+  return `Hello, ${person}! Welcome to ${city}, ${state}!`;
+}
+
+//Beginner Series #2 Clock
+
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
