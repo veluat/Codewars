0765ec53f38f85155ab29bdbd3ed03c306f805e5
Julia Popova <109670861+veluat@users.noreply.github.com>
2022-08-22 00:25:52 +0300

    Add 6 solutions

--- solutions_8kyu_part3.js
+++ solutions_8kyu_part3.js
@@ -17,3 +17,34 @@ function sayHello( name, city, state ) {
 function past(h, m, s) {
   return h * 3600000 + m * 60000 + s * 1000;
 }
+
+//Take the Derivative
+
+function derive(coefficient,exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
+
+//The Wide-Mouthed frog!
+
+function mouthSize(animal) {
+  return animal.toLowerCase().match("alligator") ? "small" : "wide";
+}
+
+//Grasshopper - Grade book
+
+function getGrade (s1, s2, s3) {
+  
+  let middle = (s1 + s2 + s3) / 3;
+
+  if ( middle >= 90 ) {
+    return "A";
+  } else if ( middle >= 80) {
+    return "B";
+  } else if ( middle >= 70 ) {
+    return "C";
+  } else if ( middle >= 60 ) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
